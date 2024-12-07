67a439110f96147d049726f8e598d24842d0c318
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-12-06 19:17:41 -0800

    add app bar to chat and add drawer

--- lib/features/chat/presentation/views/chat_view.dart
+++ lib/features/chat/presentation/views/chat_view.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:open_ai_app/core/utils/styless.dart';
 
-class ChatView extends StatelessWidget {
+import 'widgets/custom_drawer.dart';
+
+class ChatView extends StatefulWidget {
   const ChatView({super.key});
 
   @override
+  State<ChatView> createState() => _ChatViewState();
+}
+
+class _ChatViewState extends State<ChatView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      key: scaffoldKey,
+      drawer: const CustomDrawer(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('AI Chat', style: Styless.textMedium20),
+        leading: IconButton(
+          onPressed: () {
+            scaffoldKey.currentState!.openDrawer();
+          },
+          icon: const Icon(Icons.menu),
+        ),
+      ),
+    );
   }
 }
