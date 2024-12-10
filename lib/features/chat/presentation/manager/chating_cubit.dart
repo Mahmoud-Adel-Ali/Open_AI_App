f0829ce54ef65344747575fb45940554a4ea0b6c
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-12-10 16:21:59 +0200

    setup image picker package and create text ediiting controller

--- lib/features/chat/presentation/manager/chating_cubit.dart
+++ lib/features/chat/presentation/manager/chating_cubit.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 
 import 'chating_state.dart';
 
 class ChatingCubit extends Cubit<ChatingState> {
   ChatingCubit() : super(ChatingInitial());
+  TextEditingController chatTextFeild = TextEditingController();
+  List<XFile>? imagesList = [];
 }
--- lib/features/chat/presentation/views/widgets/send_message_section.dart
+++ lib/features/chat/presentation/views/widgets/send_message_section.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:open_ai_app/core/theme/theme_cubit.dart';
+import 'package:open_ai_app/features/chat/presentation/manager/chating_cubit.dart';
 
 import 'custom_text_form_field.dart';
 
@@ -17,6 +20,7 @@ class SendMessageSection extends StatelessWidget {
           const SizedBox(width: 12),
           Expanded(
             child: CustomTextFormField(
+              controller: context.read<ChatingCubit>().chatTextFeild,
               hintTxt: 'Type your message here...',
               suffix: IconButton(
                 onPressed: () {
