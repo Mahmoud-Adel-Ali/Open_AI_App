6e2909c937bf4be75e600889ca144a3816e96d59
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2025-01-03 14:15:13 +0200

    create chat list view bloc builder

--- lib/features/chat/presentation/views/widgets/chat_list_view.dart
+++ lib/features/chat/presentation/views/widgets/chat_list_view.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:open_ai_app/features/chat/presentation/manager/chating_cubit.dart';
 import 'package:open_ai_app/features/chat/presentation/views/widgets/chat_list_view_item.dart';
 
 import '../../../data/models/chat_model.dart';
+import '../../manager/chating_state.dart';
 
 class ChatListView extends StatelessWidget {
   const ChatListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return ChatListViewItem(
-          chatModel: ChatModel(
-            chatId: '',
-            message: 'message user ya pash',
-            response: 'response ai ya gale',
-            imagesUrls: [],
-            dateTime: DateTime.now(),
-          ),
+    return BlocBuilder<ChatingCubit, ChatingState>(
+      builder: (context, state) {
+        List<ChatModel> items = context.read<ChatingCubit>().currentChat;
+        return ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return ChatListViewItem(
+              chatModel: items[index],
+            );
+          },
         );
       },
     );
