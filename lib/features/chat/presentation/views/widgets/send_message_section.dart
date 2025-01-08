b465372e2953960047fdb02b6ee59282c0f6b1f3
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2025-01-08 13:32:12 +0200

    validate send message field

--- lib/features/chat/presentation/views/widgets/send_message_section.dart
+++ lib/features/chat/presentation/views/widgets/send_message_section.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../manager/chating_cubit.dart';
 import 'custom_text_form_field.dart';
 import 'pick_image_icon.dart';
