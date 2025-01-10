89d1a912aaa76b59e6526c5e224b9e01c3c4b0a2
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2025-01-10 13:59:55 +0200

    refcator open ai app file

--- lib/open_ai_app.dart
+++ lib/open_ai_app.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:open_ai_app/features/splash/presentation/view/splash_view.dart';
-
 import 'core/theme/app_theme.dart';
 import 'core/theme/theme_cubit.dart';
 import 'core/theme/theme_state.dart';
 import 'features/chat/presentation/manager/chating_cubit.dart';
+import 'features/splash/presentation/view/splash_view.dart';
 
 class OpenAiApp extends StatelessWidget {
   const OpenAiApp({super.key});
