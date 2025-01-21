ee2b35bfa89ef104b23e24d022a181d2494358d6
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-01-21 12:18:06 -0800

    handle scroll to the bottom of list view directly

--- lib/features/chat/presentation/views/widgets/chat_list_view.dart
+++ lib/features/chat/presentation/views/widgets/chat_list_view.dart
@@ -7,15 +7,43 @@ import 'package:open_ai_app/features/chat/presentation/views/widgets/custom_load
 import '../../../data/models/chat_model.dart';
 import '../../manager/chating_state.dart';
 
-class ChatListView extends StatelessWidget {
+class ChatListView extends StatefulWidget {
   const ChatListView({super.key});
+
+  @override
+  State<ChatListView> createState() => _ChatListViewState();
+}
+
+class _ChatListViewState extends State<ChatListView> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose(); // Dispose the controller
+    super.dispose();
+  }
+
+  void _scrollToBottom() {
+    // Make sure the list is rendered before scrolling
+    Future.delayed(const Duration(milliseconds: 100), () {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ChatingCubit, ChatingState>(
       builder: (context, state) {
         List<ChatModel> items = context.read<ChatingCubit>().currentChat;
+        if (items.isNotEmpty) {
+          _scrollToBottom();
+        }
         return CustomScrollView(
+          controller: _scrollController,
           slivers: [
             SliverList.builder(
               itemCount: items.length,
@@ -26,7 +54,7 @@ class ChatListView extends StatelessWidget {
               },
             ),
             state is SendMessageToAiLoading
-                ? const SliverFillRemaining(child: CustomLoadingSection())
+                ? const SliverToBoxAdapter(child: CustomLoadingSection())
                 : const SliverToBoxAdapter(),
           ],
         );
