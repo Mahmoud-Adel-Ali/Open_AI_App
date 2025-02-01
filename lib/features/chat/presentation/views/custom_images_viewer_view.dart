fa53ec7c6fb177d29510db00514332ec86b2b23a
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-01 12:18:33 -0800

    images viewer body part one

--- lib/features/chat/presentation/views/custom_images_viewer_view.dart
+++ lib/features/chat/presentation/views/custom_images_viewer_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 
 class CustomImagesViewerView extends StatelessWidget {
   const CustomImagesViewerView({
@@ -12,6 +14,31 @@ class CustomImagesViewerView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Images Viewer'),
       ),
+      body: ImageViewerBody(imagesUrls: imagesUrls),
+    );
+  }
+}
+
+class ImageViewerBody extends StatelessWidget {
+  const ImageViewerBody({super.key, required this.imagesUrls});
+  final List<String> imagesUrls;
+
+  @override
+  Widget build(BuildContext context) {
+    return PhotoViewGallery.builder(
+      enableRotation: true,
+      backgroundDecoration:
+          BoxDecoration(color: Theme.of(context).colorScheme.primary),
+      itemCount: imagesUrls.length,
+      scrollPhysics: const BouncingScrollPhysics(),
+      builder: (context, index) {
+        return PhotoViewGalleryPageOptions(
+          imageProvider: AssetImage(imagesUrls[index]),
+          initialScale: PhotoViewComputedScale.contained * 0.99,
+          heroAttributes: PhotoViewHeroAttributes(tag: index),
+          filterQuality: FilterQuality.high,
+        );
+      },
     );
   }
 }
