31249e116f05508034a77f24c74f9d4ef8d7b9a3
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-01 00:41:40 -0800

    create show toast message methode

---
+++ lib/core/widgets/toast_message.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+showToastMessage(
+  BuildContext context, {
+  required String msg,
+  MessageType type = MessageType.error,
+}) {
+  return Fluttertoast.showToast(
+      msg: "This is Center Short Toast",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: getColor(type),
+      textColor: Colors.white,
+      fontSize: 18.0);
+}
+
+getColor(MessageType type) {
+  switch (type) {
+    case MessageType.success:
+      return Colors.green[900];
+    case MessageType.error:
+      return Colors.red[900];
+    case MessageType.warning:
+      return Colors.yellow[700];
+    case MessageType.info:
+      return Colors.blue[900];
+  }
+}
+
+enum MessageType { success, error, warning, info }
