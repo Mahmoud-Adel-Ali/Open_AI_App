ac5ec8e893f50949f6cdde5489f26285f7948a04
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-02 12:24:22 -0800

    refactor custom images list view

--- lib/features/chat/presentation/views/widgets/custom_images_list_view.dart
+++ lib/features/chat/presentation/views/widgets/custom_images_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:open_ai_app/core/widgets/toast_message.dart';
 import '../../manager/chatting_cubit.dart';
 import 'custom_selected_image_box.dart';
 
@@ -21,6 +22,14 @@ class CustomImagesListView extends StatelessWidget {
                   padding: const EdgeInsets.all(8.0),
                   child: InkWell(
                       onTap: () {
+                        showToastMessage(
+                          context,
+                          msg:
+                              'If you need to remove this image .\n"Double Click"',
+                          type: MessageType.info,
+                        );
+                      },
+                      onDoubleTap: () {
                         context
                             .read<ChattingCubit>()
                             .deleteImage(images![index]);
