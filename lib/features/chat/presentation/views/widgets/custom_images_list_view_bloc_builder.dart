dd58f5bf5d2688f2ee8869e1dbc2ec4f50d2c17b
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-04 00:08:09 -0800

    hiden custom images list view while loading

--- lib/features/chat/presentation/views/widgets/custom_images_list_view_bloc_builder.dart
+++ lib/features/chat/presentation/views/widgets/custom_images_list_view_bloc_builder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../manager/chatting_cubit.dart';
 import '../../manager/chatting_state.dart';
 import 'custom_images_list_view.dart';
@@ -11,8 +12,10 @@ class CustomImagesListViewBlocBuilder extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ChattingCubit, ChattingState>(
       builder: (context, state) {
-        return CustomImagesListView(
-            images: context.read<ChattingCubit>().imagesList);
+        return state is SendMessageToAiLoading
+            ? const SizedBox()
+            : CustomImagesListView(
+                images: context.read<ChattingCubit>().imagesList);
       },
     );
   }
--- lib/features/chat/presentation/views/widgets/custom_loading_section.dart
+++ lib/features/chat/presentation/views/widgets/custom_loading_section.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+
 import '../../manager/chatting_cubit.dart';
 import 'ai_loading_message.dart';
 import 'user_message.dart';
 
 class CustomLoadingSection extends StatelessWidget {
   const CustomLoadingSection({super.key});
+  List<String> getList(List<XFile> images) {
+    List<String> urls = [];
+    for (var image in images) {
+      urls.add(image.path);
+    }
+    return urls;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         UserMessage(
-            message: context.read<ChattingCubit>().message,
-            dateTime: DateTime.now(),),
+          message: context.read<ChattingCubit>().message,
+          dateTime: DateTime.now(),
+          imagesUrls: getList(context.read<ChattingCubit>().imagesList),
+        ),
         const AILoadingMessage(),
       ],
     );
